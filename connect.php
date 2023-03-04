a064f12037bb070eaa4cfba675b3f2e5a1282c9b
Zwe Htet Htoon Oo <50207296+zwehtethtoonoo@users.noreply.github.com>
2023-03-05 03:36:36 +0630

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php 
+date_default_timezone_set('Asia/Yangon');
+// error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL ^ E_WARNING);
+
+$connect = mysqli_connect("localhost","root","","ladylove");
+
+if (!$connect) 
+{
+    echo "connection fail" . mysqli_connect_error($connect);
+}; 
+
+?>
---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/logo.jpg">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- Datatable -->
+    <link href="./vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/newstyle.css">    
+    <!--**********************************
+        Scripts
+    ***********************************-->
+        <!-- Required vendors -->
+    <script src="./vendor/global/global.min.js"></script>
+    <script src="./js/quixnav-init.js"></script>
+    <script src="./js/custom.min.js"></script>
+    <?php
+   include('connect.php');
+    
+   if (isset($_POST['btncreate'])) {
+        $DateCreated = date('Y-m-d');
+        $CustomerName = $_POST['txtCustomerName'];
+        $Shoulder = $_POST['numShoulder'];
+        $SleeveDepth = $_POST['numSleeveDepth'];
+        $WaistLine = $_POST['numWaistLine'];
+        $HipLine = $_POST['numHipLine'];
+        $BackWidth = $_POST['numBackWidth'];
+        $FrontWidth = $_POST['numFrontWidth'];
+        $Bust = $_POST['numBust'];
+        $Waist = $_POST['numWaist'];
+        $Hip = $_POST['numHip'];
+        $BustLength = $_POST['numBustLength'];
+        $BustPoint = $_POST['numBustPoint'];
+        $Neck = $_POST['numNeck'];
+        $ArmHole = $_POST['numArmHole'];
+        $SleeveWidth = $_POST['txtSleeveWidth'];
+        $SleeveLength = $_POST['numSleeveLength'];
+        $PhoneNumber = $_POST['txtPhoneNumber'];
+        
+        $insert = "Insert into customer(CustomerName,Shoulder,SleeveDepth,WaistLine,HipLine,BackWidth,FrontWidth,Bust,Waist,Hip,BustLength,BustPoint,Neck,ArmHole,SleeveWidth,SleeveLength,PhoneNumber,DateCreated) values ('$CustomerName','$Shoulder','$SleeveDepth','$WaistLine','$HipLine','$BackWidth','$FrontWidth','$Bust','$Waist','$Hip','$BustLength','$BustPoint','$Neck','$ArmHole','$SleeveWidth','$SleeveLength','$PhoneNumber','$DateCreated')";
+
+        $query = mysqli_query($connect,$insert);
+        if ($query) {
+           echo "<script>alert('New Customer is successfully created! ')</script> ";
+        }
+        else 
+        {
+            echo "<script>alert('Something Went Wrong! Pls Try Again! ".$error."')</script> ";
+        };
+   }
+    ?>
+
+</head>
+<body>
+    <!-- Nav -->
+    <nav>
+        <img src="images/logo.jpg">
+        <div>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Manage</a></li>
+            <li><a href="">Home</a></li>
+        </div>
+    </nav>
+    <!-- end of nav -->
+    <!-- Sec -->
+    <section class="customer">
+      <form action="index.php" method="post" >
+        <h2 align="middle">Customer Entry</h2>
+        <p>Please fill in this form to create a customer.</p>
+        <hr>
+        <div class="container">
+            <label class="measurements"><b>Customer name</b></label>
+            <input type="text" name="txtCustomerName" style="width:100%; text-align:left;"placeholder="Enter customer name..." required>
+
+            <label class="measurements"><b>Shoulder</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numShoulder" required>
+
+            <label class="measurements"><b>Sleeve Depth</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numSleeveDepth" required>
+
+            <label class="measurements"><b>Waist Line</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numWaistLine"  required>
+
+            <label class="measurements"><b>Hip Line</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numHipLine" required>
+
+            <label class="measurements"><b>Back Width</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numBackWidth"  required>
+
+            <label class="measurements"><b>Front Width</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numFrontWidth"  required>
+
+            <label class="measurements"><b>Bust</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numBust"  required>
+
+            <label class="measurements"><b>Waist</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numWaist"  required>
+
+            <label class="measurements"><b>Hip</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numHip"  required>
+
+            <label class="measurements"><b>Bust Length</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numBustLength"  required>
+
+            <label class="measurements"><b>Bust Point</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numBustPoint"  required>
+
+            <label class="measurements"><b>Neck</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numNeck"  required>
+
+            <label class="measurements"><b>ArmHole</b></label>
+            <input type="text" minlength="2" maxlength="4" name="numArmHole"  required>
+
+            <label class="measurements"><b>SleeveWidth</b></label>
+            <input type="text" name="txtSleeveWidth"   required>
+
+            <label class="measurements"><b>SleeveLength</b></label>
+            <input type="text" name="numSleeveLength"  required>
+
+            <label class="measurements"><b>Phone Number</b></label>
+            <input type="text" name="txtPhoneNumber" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" minlength="8" maxlength="11"  placeholder="09**********" required>
+
+            <input type="submit" name="btncreate" class="btn" value="Submit">
+        </div>
+      </form>
+    </section>
+
+<script>
+    $(document).ready(function () {
+  $('#dtBasicExample').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+});
+</script>
+    <div class="app">
+        <div class="text-box">
+            <h3 style="padding-left: 1rem;">Customers list</h3>
+        </div>
+
+        <div class="list-box">
+        <table id="dtBasicExample" class="table table-bordered table-sm" cellspacing="0" width="98%">
+  <thead>
+    <tr>
+      <th class="th-sm">Name
+      </th>
+      <th class="th-sm">Bust
+      </th>
+      <th class="th-sm">Waist
+      </th>
+      <th class="th-sm">Hip
+      </th>
+      <th class="th-sm">Action
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php 
+
+        $select="Select CustomerID, CustomerName, Bust, Waist, Hip 
+                from customer;";
+        $run=mysqli_query($connect,$select);
+        $count=mysqli_num_rows($run);
+
+        for ($i=0; $i < $count; $i++) { 
+          $row=mysqli_fetch_array($run); 
+?>
+    <tr>
+      <td><?php echo $row['CustomerName'];?></td>
+      <td><?php echo $row['Bust'];?></td>
+      <td><?php echo $row['Waist'];?></td>
+      <td><?php echo $row['Hip'];?></td>
+      <td>
+          <span>
+              <a href="customer.php?cusid=<?php echo  $row['CustomerID']; ?>" data-toggle="tooltip" data-placement="top" title="View">
+                  <img src="images/eye.png" class="eye" >
+              </a>
+          </span>
+      </td>
+
+    </tr>
+
+
+       <?php }
+  ?>
+    
+    </tbody>
+  <tfoot>
+    <tr>
+      <th>Name
+      </th>
+      <th>Bust
+      </th>
+      <th>Waist
+      </th=>
+      <th>Hip
+      </th=>
+      <th>Action
+      </th>
+    </tr>
+  </tfoot>
+</table>
+
+        </div>
+    </div>
+    <!-- Jquery Validation -->
+    <script src="./vendor/jquery-validation/jquery.validate.min.js"></script>
+    <!-- Form validate init -->
+    <script src="./js/plugins-init/jquery.validate-init.js"></script>
+
+    <!-- Datatable -->
+    <script src="./vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="./js/plugins-init/datatables.init.js"></script>
+</body>
+</html>
